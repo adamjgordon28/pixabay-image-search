7f7c9e0a54ca2341e1ad31d7cfd62a92f8c4d407
Adam Gordon <adamjgordon24@gmail.com>
2022-12-06 01:21:51 -0500

    final commit: 12-06-22 1:24AM EST

--- src/pages/ImageView/ImageView.js
+++ src/pages/ImageView/ImageView.js
@@ -19,28 +19,37 @@ const ImageView = () => {
 
   return (
     <div className="image-view">
-      <button onClick={returnToSearchClickHandler}>Return to Search</button>
+      <button onClick={returnToSearchClickHandler}>
+        {"< Return to Search"}
+      </button>
       <div className="image-view-title-container">
         <span>View Image</span>
       </div>
-      <div className="user-tile">
-        <div className="user-tile-image-container">
-          <img alt="" src={image?.userImageURL} />
-          <span>Posted by: {image?.user}</span>
-        </div>
-      </div>
-      <div className="main-image-container">
-        <img alt="" src={image?.largeImageURL} />
-      </div>
-      <div className="image-tags-container">
-        <span>Tags: </span>
-        {imageTags?.map((tag) => {
-          return (
-            <div className="image-tag" key={tag}>
-              {tag}
+
+      <div className="main-image-section-container">
+        <div className="main-image-section">
+          <div className="main-image-info-section">
+            <div className="user-tile">
+              <div className="user-tile-image-container">
+                <img alt="" src={image?.userImageURL} />
+                <span>Posted by: {image?.user}</span>
+              </div>
             </div>
-          );
-        })}
+            <div className="image-tags-container">
+              <span>Tags: </span>
+              {imageTags?.map((tag) => {
+                return (
+                  <div className="image-tag" key={tag}>
+                    {tag}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="main-image">
+            <img alt="" src={image?.largeImageURL} />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 export const imageSearch = async ({ query }) => {
   const response = await fetch(
-    `https://pixabay.com/api/?key=31860958-1f3c5e322d88dd0cee36f1584&q=${query}&page=1&per_page=50`
+    `https://pixabay.com/api/?key=31860958-1f3c5e322d88dd0cee36f1584&q=${query}&page=1&per_page=100`
   );
   return await response.json();
 };
